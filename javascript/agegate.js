ec30b44e08410544a8ca72d58d84d89b7805b695
Rob Rankin <rob.rankin@revolutionstrategy.com>
2018-11-27 11:02:10 -0400

    Wait for document before binding form events

--- javascript/agegate.js
+++ javascript/agegate.js
@@ -1,27 +1,29 @@
-var form = $('.AgeGate form');
-$(':submit', form).click(function() {
-    if ($(this).attr('name')) {
-        $(form).append(
-            $("<input type='hidden'>").attr({
-                name: $(this).attr('name'),
-                value: $(this).attr('value')
-            })
-        );
-    }
-});
-$(form).submit(function(e) {
-    e.preventDefault();
-    $.ajax({
-        type: form.attr('method'),
-        url: form.attr('action'),
-        data: form.serialize()
-    })
-    .done(function (response) {
-        var result = JSON.parse(response);
-        if (result.success) {
-            $('.AgeGate').fadeOut();
-        } else {
-            window.location = result.redirect;
+$(document).ready(function() {
+    var form = $('.AgeGate form');
+    $(':submit', form).click(function() {
+        if ($(this).attr('name')) {
+            $(form).append(
+                $("<input type='hidden'>").attr({
+                    name: $(this).attr('name'),
+                    value: $(this).attr('value')
+                })
+            );
         }
+    });
+    $(form).submit(function(e) {
+        e.preventDefault();
+        $.ajax({
+            type: form.attr('method'),
+            url: form.attr('action'),
+            data: form.serialize()
+        })
+        .done(function (response) {
+            var result = JSON.parse(response);
+            if (result.success) {
+                $('.AgeGate').fadeOut();
+            } else {
+                window.location = result.redirect;
+            }
+        });
     });
 });
